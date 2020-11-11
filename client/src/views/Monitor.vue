f7a2047dcc3842cf40ff64fdbaf3ecd4f4026ffa
antonio caccamo <caccamo.antonio@gmail.com>
2020-11-11 18:12:48 +0100

    #3 electron typescript

---
+++ client/src/views/Monitor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-alert variant="info" show>
+      {{ monitor }}
+    </b-alert>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import { BAlert, BButton } from "bootstrap-vue";
+
+@Component({
+  components: {
+    BAlert,
+    BButton,
+  },
+})
+export default class Monitor extends Vue {
+  private monitor = "";
+
+  mounted() {
+    console.log(this.$route.params);
+    this.monitor = this.$route.path;
+  }
+}
+</script>
+
+<style scoped>
+</style>
